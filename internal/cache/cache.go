802bbecdbe6b6f2c7fc090f0bedd785478201152
mluffman <mluffman@thoughtmachine.net>
2023-12-20 10:24:31 +0000

    add additional tests to cache package, remove unused methods from interface

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -10,17 +10,31 @@ import (
 // Cache provides a simple item store
 type Cache interface {
 	// Insert the string k
-	Insert(k string)
+	Insert(k string, v string)
 	// Exists proves the existence or lack-thereof of the item k in the cache
 	Exists(k string) (exists bool)
+	// Get returns the value of the key in the cache
+	Get(k string) (value string, ok bool)
+	// SetExpiration sets the expiration on the given item k without updating the value
+	SetExpiration(k string, t time.Duration)
 }
+
+const NoExpiration = gocache.NoExpiration
 
 type cache struct {
 	u *gocache.Cache
 }
 
-func (c *cache) Insert(k string) {
-	c.u.SetDefault(k, struct{}{})
+func (c *cache) SetExpiration(k string, t time.Duration) {
+	e, ok := c.u.Get(k)
+	if !ok {
+		return
+	}
+	c.u.Set(k, e, t)
+}
+
+func (c *cache) Insert(k string, v string) {
+	c.u.SetDefault(k, v)
 }
 
 func (c *cache) Exists(k string) (exists bool) {
@@ -28,9 +42,31 @@ func (c *cache) Exists(k string) (exists bool) {
 	return exists
 }
 
+func (c *cache) Get(k string) (value string, ok bool) {
+	v, ok := c.u.Get(k)
+	if !ok {
+		return "", ok
+	}
+	value, ok = v.(string)
+	return value, ok
+}
+
 // NewCacheWithTTL creates a new cache with ttl of t
 func NewCacheWithTTL(t time.Duration) Cache {
 	return &cache{
 		u: gocache.New(t, time.Minute*60),
+	}
+}
+
+func NewCacheWithTTLFrom(t time.Duration, f map[string]string) Cache {
+	m := make(map[string]gocache.Item, len(f))
+	for k, v := range f {
+		m[k] = gocache.Item{
+			Object:     v,
+			Expiration: t.Nanoseconds(),
+		}
+	}
+	return &cache{
+		u: gocache.NewFrom(t, time.Minute*60, m),
 	}
 }
--- internal/cache/cache_test.go
+++ internal/cache/cache_test.go
@@ -16,9 +16,38 @@ func TestCacheTestSuite(t *testing.T) {
 }
 
 func (suite *CacheTestSuite) TestNewCacheWithTTL() {
-	c := NewCacheWithTTL(time.Second)
-	c.Insert("key")
-	suite.True(c.Exists("key"))
-	time.Sleep(time.Millisecond * 1100)
-	suite.False(c.Exists("key"))
+	expirationDuration := time.Millisecond * 500
+	c := NewCacheWithTTL(expirationDuration * 2)
+	c.Insert("key", "")
+	suite.Eventually(func() bool {
+		return c.Exists("key")
+	}, time.Second*5, expirationDuration)
+	suite.Eventually(func() bool {
+		return !c.Exists("key")
+	}, time.Second*2, expirationDuration)
+}
+
+func (suite *CacheTestSuite) TestNewCacheWithTTLFrom() {
+	expirationDuration := NoExpiration
+	existingItemKey := "item"
+	existingItemValue := "value"
+	existing := map[string]string{
+		existingItemKey: existingItemValue,
+	}
+	c := NewCacheWithTTLFrom(expirationDuration, existing)
+	v, ok := c.Get(existingItemKey)
+	suite.True(ok)
+	suite.Equal(existingItemValue, v)
+}
+
+func (suite *CacheTestSuite) TestSetExpiration() {
+	itemKey := "item"
+	c := NewCacheWithTTL(NoExpiration)
+	c.SetExpiration("non-existent", NoExpiration)
+	c.Insert(itemKey, "")
+	c.SetExpiration(itemKey, time.Nanosecond)
+	suite.Eventually(func() bool {
+		_, ok := c.Get(itemKey)
+		return !ok
+	}, time.Second*2, time.Microsecond)
 }
